7104f84c065b0ed3dbb30c1295e1951bc73fed5c
clawlynx <clawlynx77@gmail.com>
2026-03-24 13:58:27 +0530

    new backend added for aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://luminauratest.onrender.com/api/v1",
+  baseURL: "https://api.luminauraindia.com/api/v1",
   withCredentials: true,
 });
 // "https://luminauratest.onrender.com/api/v1",
 // http://localhost:3000/api/v1
+//https://api.luminauraindia.com/api/v1
